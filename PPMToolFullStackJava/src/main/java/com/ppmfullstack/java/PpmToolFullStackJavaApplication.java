1910f880daeddafc9fb637462d7727c1f4047eec
Ravi <raviktummala@gmail.com>
2021-01-09 16:09:52 +0530

    First commit backend

---
+++ PPMToolFullStackJava/src/main/java/com/ppmfullstack/java/PpmToolFullStackJavaApplication.java
@@ -0,0 +1,13 @@
+package com.ppmfullstack.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolFullStackJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolFullStackJavaApplication.class, args);
+	}
+
+}
